96fe7c5fb2b25db86e52e7e927e1d8bb4d9c0a3b
Lloyd Semblante <92197616+lloydshine@users.noreply.github.com>
2024-09-05 16:30:02 +0000

    Pending changes exported from your codespace

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,4 +1,9 @@
-import { ChevronRightCircle, PlayCircleIcon, PlusCircleIcon, StarIcon } from "lucide-react";
+import {
+  ChevronRightCircle,
+  PlayCircleIcon,
+  PlusCircleIcon,
+  StarIcon,
+} from "lucide-react";
 import { API_KEY, Movie } from "../lib/globals";
 import { useEffect, useState } from "react";
 import axios from "axios";
